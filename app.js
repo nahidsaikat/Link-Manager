46f8c1b833fc3fc9c16bdbec5efc741e256bd9da
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-08 00:21:12 +0600

    Mongo db setup for the project

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/link-manager', { useNewUrlParser: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch((err) => console.error('Something went wrong', err));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,3 +23,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+  });
