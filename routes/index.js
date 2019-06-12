73051b20fe584702ac36738a7b2f8e484a52bf48
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-12 18:57:01 +0600

    Remove footer and looping the cards

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home.html', { title: 'Link Manager' });
+  var card_list = [];
+  for (var i=0; i<20; i++) card_list.push(i);
+  res.render('home.html', { title: 'Link Manager', card_list: card_list });
 });
 
 module.exports = router;
