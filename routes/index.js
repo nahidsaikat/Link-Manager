69848db6813a9e683f6e25363628fa9a0cf8dfcc
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-12 17:59:47 +0600

    Separating index.html into base and home html file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html', { title: 'Link Manager' });
+  res.render('home.html', { title: 'Link Manager' });
 });
 
 module.exports = router;
