6677507522680fd8459c63fc6f05f12e8deeccab
nahidsaikat <nahidsaikatft40@gmail.com>
2019-06-14 12:26:21 +0600

    Making three row of the card and aligning to center

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var card_list = [];
-  for (var i=0; i<20; i++) card_list.push(i);
+  for (var i=0; i<5; i++) card_list.push([i, i, i]);
   res.render('home.html', { title: 'Link Manager', card_list: card_list });
 });
 
